bc65149cf187c1633c23de207b96bebeefd7b5a4
Tommi Kivimäki <tommi.kivimaki@caravanstudio.fi>
2020-03-16 14:33:00 +0200

    Added minimum iOS version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NetClientCombine",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
